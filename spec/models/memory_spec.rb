1019cf51b8bd8a1c347d77cec32a12d03e6d859b
Krister Vals <krister.vals@gmail.com>
2016-05-27 09:13:48 +0300

    Initial memory model with tests

---
+++ spec/models/memory_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe Memory do
+  it { should validate_attachment_presence(:image) }
+  it { should have_attached_file(:image) }
+  it { should validate_attachment_content_type(:image).allowing('image/png', 'image/gif').rejecting('text/plain', 'text/xml') }
+end
