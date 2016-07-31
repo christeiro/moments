458ca17179d3f2cb982323a34b2fd296c15840f9
Krister Vals <krister.vals@gmail.com>
2016-07-31 14:05:45 +0300

    Nested comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'moments#index'
-  resources :moments
+  resources :moments do
+    resources :comments
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
