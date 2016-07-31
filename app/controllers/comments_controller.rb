199b37b5dba3ed4ebb51ea119aebaabb2de07bab
Krister Vals <krister.vals@gmail.com>
2016-07-31 16:59:25 +0300

    Adding comments with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,10 @@ class CommentsController < ApplicationController
     @comment = @moment.comments.build(set_params)
     @comment.user_id = current_user.id
     if @comment.save
-      flash[:success] = "Your comment was added to the moment"
-      redirect_to :back
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
     else
       flash[:alert] = "Check the comment form, something went horribly wrong!"
       render root_path
